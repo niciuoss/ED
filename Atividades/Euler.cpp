072140b9520a5c8c2aaf6e45725f1033f069c270
niciuoss <viniciusamorimsales@gmail.com>
2020-11-24 18:15:58 -0300

    criação do escopo em c++

---
+++ Atividades/Euler.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <locale>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL,"");
+
+    int n = 0;
+    
+    cout << "Digite um número inteiro positivo: ";
+    cin >> n;
+
+    if ( n != 0 && n != 1 ){
+
+
+    }
+
+    return 0;
+}
+
+double fatorial(int n){
+    double valorF = 0.00;
+
+
+    return valorF;
+}
+
+double euler(int n){
+    double valorE = 0.00;
+
+    fatorial(n);
+
+    return valorE;
+}
