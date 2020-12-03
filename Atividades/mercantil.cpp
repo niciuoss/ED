d8b774d2c52cc9d850f0acd015871c5b17aed985
niciuoss <viniciusamorimsales@gmail.com>
2020-12-03 14:13:24 -0300

    Fim do mercantil

--- Atividades/mercantil.cpp
+++ Atividades/mercantil.cpp
@@ -11,6 +11,7 @@ int main (){
     double *valor;
     double *chute;
     string *segundo;
+    double *auxS;
     int i = 0;
 
     cout << "Quantos produtos: ";
@@ -31,18 +32,66 @@ int main (){
     }
     
     cout << "A resposta do segundo: ";
-    segundo = new string[2];
+    segundo = new string[5];
     for(i = 0; i < quantP; i++){
         cin >> segundo[i];
         //cout << segundo[i] << "\n";
     }
 
-    
-    
+    auxS = (double*) new int(quantP);
+
+    for(i = 0; i < quantP; i++){
+        if (segundo[i] == "M" && valor[i] > chute[i]){
+            auxS[i] = valor[i];
+        }
+        else if (segundo[i] == "M" && valor[i] < chute[i]){
+            auxS[i] = chute[i] + 0.1;
+        }
+        else if (segundo[i] == "M" && valor[i] == chute[i]){
+            auxS[i] = chute[i] + 0.1;
+        }       
+        else if (segundo[i] == "m" && valor[i] < chute[i]){
+            auxS[i] = valor[i];
+        }
+        else if (segundo[i] == "m" && valor[i] > chute[i]){
+            auxS[i] = chute[i] - 0.1;
+        }
+        else if (segundo[i] == "m" && valor[i] == chute[i]){
+            auxS[i] = chute[i] + 0.1;
+        }
+    }
+    /*for(i = 0; i < quantP; i++){
+        cout << auxS[i] << "\n";                 
+    }*/
+    int contP = 0;
+    int contS = 0;
+    int contE = 0;
+    for(i = 0; i < quantP; i++){
+        if((chute[i] - valor[i]) < (auxS[i] - valor[i])){
+            contP++;//0/1/1
+        }
+        else if ((chute[i] - valor[i]) > ( auxS[i] - valor[i])){
+            contS++;//1/0/0
+        }
+        else if (( chute[i] - valor[i]) == ( auxS[i] - valor[i])){
+            contE++;//0/0/0
+        }
+    }
+    //cout << contP << "\n";
+    //cout << contS << "\n";
+    //cout << contE << "\n";
+    if (contS > contP && contS > contE){
+        cout << "segundo" << "\n";
+    } else if (contP > contS && contP > contE){
+        cout << "primeiro" << "\n";
+    } else {
+        cout << "empate" << "\n";
+    }
 
  
  
     free(valor);
+    free(auxS);
     free(chute);
     delete []segundo;
     
