3e53bee444ae23c10fa8f247eed6c8bf8b4663c4
niciuoss <viniciusamorimsales@gmail.com>
2021-01-24 00:30:50 -0300

    correção do Ponto cpp e início do Circulo cpp

---
+++ Atividades/Circulo/Circulo.cpp
@@ -0,0 +1,49 @@
+#include "Circulo.h"
+struct Circulo; 
+
+Circulo *circ_cria(double raio, Ponto *centro){
+    
+}
+
+
+void circ_libera(Circulo *c){
+    
+}
+
+
+void circ_setRaio(Circulo *c, double raio){
+    
+}
+void circ_setCentro(Circulo *c, Ponto *p){
+    
+}
+void circ_setX(Circulo *c, double x){
+    
+} 
+void circ_setY(Circulo *c, double y){
+    
+} 
+
+
+double circ_getRaio(Circulo *c){
+    
+}
+Ponto *circ_getCentro(Circulo *c){
+    
+}
+double circ_getX(Circulo *c){
+    
+} 
+double circ_getY(Circulo *c){
+    
+} 
+
+
+double circ_getArea(Circulo *c){
+    
+}
+
+
+bool circ_interior(Circulo *c, Ponto *p){
+    
+}
--- Atividades/Circulo/Ponto.cpp
+++ Atividades/Circulo/Ponto.cpp
@@ -13,27 +13,27 @@ Ponto *pto_cria(double x, double y){
     p->y = y;
 
     return p; 
-};
+}
 
 void pto_libera(Ponto* p){
     delete[] p;
-};
+}
 
 double pto_getX(Ponto *p){
     return p->x;
-};
+}
 
 double pto_getY(Ponto *p){
     return p->y;
-};
+}
 
 void pto_setX(Ponto *p, double x){
     p->x = x;
-};
+}
 
 void pto_setY(Ponto *p, double y){
     p->y = y;
-};
+}
 
 double pto_distancia(Ponto* p1, Ponto* p2){
     double resultado = 0.0;
@@ -43,5 +43,5 @@ double pto_distancia(Ponto* p1, Ponto* p2){
     double yb = p2->y;
     
     resultado = sqrt(pow(xb-xa, 2) + pow(yb-ya,2));
-};
+}
 
